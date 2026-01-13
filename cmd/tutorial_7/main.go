00a2377c5febfde136b85f9cf926df1d86200a79
Rashed Al Maaitah <bahran568@gmail.com>
2026-01-13 20:20:45 +0300

    tutorial_7, channel, select statement

---
+++ cmd/tutorial_7/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const MAX_CHICKEN_PRICE float32 = 5
+const MAX_TOFU_PRICE float32 = 3
+
+func main() {
+	var chickenChannel = make(chan string)
+	var tofuChannel = make(chan string)
+	var websites = []string{"wallmart.com", "target.com", "bestbuy.com"}
+	for i := range websites {
+		go checkChickenPrices(websites[i], chickenChannel)
+		go checkTofuPrices(websites[i], tofuChannel)
+	}
+	sendMessage(chickenChannel, tofuChannel)
+}
+
+func checkTofuPrices(website string, tofuChannel chan string) {
+	for {
+		time.Sleep(time.Second * 1)
+		var tofuPrice = rand.Float32() * 10
+		if tofuPrice <= MAX_TOFU_PRICE {
+			tofuChannel <- website
+			break
+		}
+	}
+}
+func checkChickenPrices(website string, chickenChannel chan string) {
+	for {
+		time.Sleep(time.Second * 1)
+		var chickenPrice = rand.Float32() * 20
+		if chickenPrice <= MAX_CHICKEN_PRICE {
+			chickenChannel <- website
+			break
+		}
+	}
+}
+
+func sendMessage(chickenChannel chan string, tofuChannel chan string) {
+	select {
+	case website := <-chickenChannel:
+		fmt.Printf("Found a deal on chicken at: %s\n", website)
+	case website := <-tofuChannel:
+		fmt.Printf("Found a deal on tofu at: %s\n", website)
+	}
+}
